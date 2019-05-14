ac64c28b0bc88aa10b417c81c5bd57416f860b3f
ChrisWen <chriswen@ChrisWendeMacBook-Pro.local>
2019-05-14 09:10:07 +0800

    Modify readme

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -1,6 +1,10 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+// import "core-js/fn/array.find"
+// ...
 export default class DummyClass {
 
+}
+
+function getArgs (...args: string[]) {
+  return args;
 }
